eadb655cff9f1ea00c5de75cbc6c823317a4250f
PitonPeton <magnothrilex@gmail.com>
2023-06-07 16:32:19 -0400

    first

---
+++ shop.Domain/Repository/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using shop.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace shop.Domain
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ shop.Infraestructure/Context/shopContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace shop.Infraestructure.Context
+{
+    public class shopContext : Dbcontext
+    {
+        public shopContext() { }
+        public shopContext(
+            Dbcontext options <shopContext> options : options
+            )
+        { }
+
+    }
+}
