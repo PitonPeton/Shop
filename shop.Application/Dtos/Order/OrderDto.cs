6eed01712b87d588ae7f049c456adf86a9eb2a9c
Daytson Bobadilla <daytsonbobadilla@outlook.com>
2023-06-21 00:31:51 -0400

    Orders OK

--- shop.Application/Dtos/Order/OrderDto.cs
+++ shop.Application/Dtos/Order/OrderDto.cs
@@ -4,7 +4,10 @@ namespace shop.Application.Dtos.Order
 {
     public abstract class OrderDto : DtoBase
     {
-        public int freight { get; set; }
+        public int? custid { get; set; }
+        public int empid { get; set; }
+        public int shipperid { get; set; }
+        public decimal freight { get; set; }
         public DateTime requireddate { get; set; }
         public DateTime orderdate { get; set; }
         public int creation_user { get; set; }
--- shop.Domain/Entities/Orders.cs
+++ shop.Domain/Entities/Orders.cs
@@ -10,9 +10,34 @@ namespace shop.Domain.Entities.Orders
     {
         [Key]
         public int orderid { get; set; }
-        public int freight { get; set; }
+
+        [ForeignKey("Customers")]
+        public int? custid { get; set; }
+
+        [ForeignKey("Employee")]
+        public int empid { get; set; }
+        public int shipperid { get; set; }
+        public decimal freight { get; set; }
         public DateTime requireddate { get; set; }
         public DateTime orderdate { get; set; }
 
+    }
+
+    [Table("Employees", Schema = "HR")]
+    public class Employee    {
+        public int empid { get; set; }
+        public string lastname { get; set; }
+        public string firstname { get; set; }
+        public string title { get; set; }
+        public string titleofcourtesy { get; set; }
+        public DateTime birthdate { get; set; }
+        public DateTime hiredate { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string region { get; set; }
+        public string postalcode { get; set; }
+        public string country { get; set; }
+        public string phone { get; set; }
+        public int? mgrid { get; set; }
     }
 }
