205c5eb1d9510d9f043354cde6a70b46b201e35e
Jose Angel Dipre Mateo <dipre.jose08@gmail.com>
2023-07-30 19:43:01 -0400

    Capa Dominio

---
+++ shop.Domain/Core/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace shop.Domain.Core
+{
+    public abstract class BaseEntity
+    {
+        public BaseEntity() 
+        {
+            this.creation_date = DateTime.Now;
+            this.deleted = false;
+        }
+        public DateTime creation_date { get; set; }
+        public int creation_user { get; set; }
+        public DateTime? modify_date { get; set; }
+        public int? modify_user { get; set; }
+        public int? delete_user { get; set; }
+        public DateTime? delete_date { get; set; }
+        public bool deleted { get; set; }
+    }
+}
---
+++ shop.Domain/Entities/Customer.cs
@@ -0,0 +1,25 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace shop.Domain.Entities.Customer
+{
+    [Table("Customers", Schema = "Sales")]
+    public partial class Customer : Core.BaseEntity
+    {
+        [Key]
+        public int custid { get; set; }
+        public string companyname { get; set; }
+        public string contactname { get; set; }
+        public string contacttitle { get; set; }
+        public string address { get; set; }
+        public string email { get; set; }
+        public string city { get; set; }
+        public string? region { get; set; }
+        public string? postalcode { get; set; }
+        public string country { get; set; }
+        public string phone { get; set; }
+        public string? fax { get; set; }
+
+    }
+}
